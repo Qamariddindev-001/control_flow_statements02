e40646a2db1c066b3d1d6c7fbbac6c661b84e812
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-07 12:19:18 +0500

    bin/if02.dart else

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,23 @@ Args:
 Returns:
     int: return answer.
 */
+int func(
+  int a,
+  int b,
+  int c,
+) {
+  int minn = 0;
+  if (a < b && a < c) {
+    minn = a;
+  } else if (b < a && b < c) {
+    minn = b;
+  } else if (c < a && c < b) {
+    minn = c;
+  }
+
+  return minn;
+}
+
 void main() {
+  print(func(22, 44, 77));
 }
