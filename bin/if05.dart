e29c797311a0b9b000ad7c075aa7a4a4244a8e87
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-07 14:37:50 +0500

    bin/if05.dart else

--- bin/if05.dart
+++ bin/if05.dart
@@ -3,9 +3,26 @@ Create function called func
 Find the largest digit of the four-digit number.
 Args:
     n: Five-digit number.
-Returns:
+Returns:6780
     int: return answer.
 */
+int func(int n) {
+  int x4 = n % 10;
+  int x3 = n ~/ 10 % 10;
+  int x2 = n ~/ 100 % 10;
+  int x1 = n ~/ 1000;
+  int maxx = x4;
+  if ((x3) > maxx) {
+    maxx = x3;
+  } else if ((x2) > maxx) {
+    maxx = x2;
+  }
+  if ((x1) > maxx) {
+    maxx = x1;
+  }
+  return maxx;
+}
 
 void main() {
+  print(func(5400));
 }
