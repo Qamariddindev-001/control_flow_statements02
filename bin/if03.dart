6c749bd91bd8c8759a9568c50d09969fdeb0212e
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-07 12:56:50 +0500

    bin/if03.dart else

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,29 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+
+int func(int a, int b, int c) {
+  int summ = 0;
+  int minn = 0;
+  int maxx = 0;
+  if (a < b && a < c) {
+    minn = a;
+  } else if (b < a && b < c) {
+    minn = b;
+  } else if (c < a && c < b) {
+    minn = c;
+  }
+  if (a > b && a > c) {
+    maxx = a;
+  } else if (b > a && b > c) {
+    maxx = b;
+  } else if (c > a && c > b) {
+    maxx = c;
+  }
+  summ = a + b + c;
+  return summ - maxx - minn;
+}
+
 void main() {
+  print(func(4, 3, 5));
 }
